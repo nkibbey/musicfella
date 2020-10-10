c7723ddb53df95bf3eb0153d82f1427e68a38fa4
nk <nkibbey@terpmail.umd.edu>
2020-10-10 17:12:47 -0400

    add album struct

--- main.go
+++ main.go
@@ -2,9 +2,20 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/nkibbey/musicfella/read"
 )
+
+// Album defines info to aid musicfella users to identify a real life album
+type Album struct {
+	Name    string
+	Artist  string
+	Rdate   time.Time
+	Links   []string
+	Genre   []string
+	Country string
+}
 
 func main() {
 	fmt.Printf("Hello \n")
